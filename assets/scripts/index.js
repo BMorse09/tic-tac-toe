0060750ea19676e4cf6213b3eba3fa26234e33f1
BMorse09 <benmorse09@gmail.com>
2016-09-26 23:20:11 -0400

    asasdfasdfas

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,20 @@
+
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+// load manifests
+// scripts
+require('./assets/scripts/index.js');
+
+// styles
+require('./assets/styles/index.scss');
+
+// attach jQuery globally
+require('expose?$!jquery');
+require('expose?jQuery!jquery');
+
+// attach getFormFields globally
+
+require('expose?getFormFields!./lib/get-form-fields.js');
